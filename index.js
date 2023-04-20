d29fd907e2e5e76467b332a5a2fa4ceda781a7ca
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2023-04-20 11:13:11 +0330

    ben formattato...

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const celsiusFahrenheitKelvin = temp => (!/[cfk]/i.test(temp[temp.length - 1]) || /[cfk]/i.test(temp[0]) || temp[0] === ' ') ? new Error('inserimento non valido; il formato valido è #C (or #c) or #F (or #f) or #K (or #k).') : (/c/i.test(temp) ? `"${(temp.replace(/c/i, '') * (9 / 5) + 32)} °F" e "${(temp.replace(/c/i, '') - (-273.15))} °K"` : ((/f/i.test(temp) ? `"${((temp.replace(/f/i, '') - 32) / 1.8)} °C" e "${(temp.replace(/f/i, '') - 32) / 1.8 + 273.15} °K"` : `"${(temp.replace(/k/i, '') - 273.15)} °C" e "${1.8 * (temp.replace(/k/i, '') - 273.15) + 32} °F"`)));
+const celsiusFahrenheitKelvin = (temp) =>
+  !/[cfk]/i.test(temp[temp.length - 1]) ||
+  /[cfk]/i.test(temp[0]) ||
+  temp[0] === ' '
+    ? new Error(
+        'inserimento non valido; il formato valido è #C (or #c) or #F (or #f) or #K (or #k).'
+      )
+    : /c/i.test(temp)
+    ? `"${temp.replace(/c/i, '') * (9 / 5) + 32} °F" e "${
+        temp.replace(/c/i, '') - -273.15
+      } °K"`
+    : /f/i.test(temp)
+    ? `"${(temp.replace(/f/i, '') - 32) / 1.8} °C" e "${
+        (temp.replace(/f/i, '') - 32) / 1.8 + 273.15
+      } °K"`
+    : `"${temp.replace(/k/i, '') - 273.15} °C" e "${
+        1.8 * (temp.replace(/k/i, '') - 273.15) + 32
+      } °F"`;
 
 export default celsiusFahrenheitKelvin;
